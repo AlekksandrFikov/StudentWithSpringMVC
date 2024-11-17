c0394c390306d050a105c8f45dca297195b73064
Alekksandr Fikov <cupofcoffee1979@gmail.com>
2024-11-17 22:31:50 +0300

    Prepare for merging

--- src/main/java/ru/kors/springstudents/SpringStudentsApplication.java
+++ src/main/java/ru/kors/springstudents/SpringStudentsApplication.java
@@ -1,10 +1,7 @@
 package ru.kors.springstudents;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 
 @SpringBootApplication
 public class SpringStudentsApplication {
@@ -14,15 +11,8 @@ public class SpringStudentsApplication {
 	public static void main(String[] args) {
 	
 		SpringApplication.run(SpringStudentsApplication.class, args);
-//		System.out.println("1");
-//	
-//		assertFalse(true);
-//
-//		System.out.println("2");
+
 	} 
 
-	@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
-	void d () {
-		
-	}
+
 }
